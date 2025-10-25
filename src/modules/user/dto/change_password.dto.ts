c656298503eb01c6f9727e3eec8dedb07af0aefa
Dang Tran <15910@student.vgu.edu.vn>
2025-10-25 17:56:44 +0700

    Change language into English

--- src/modules/user/dto/change_password.dto.ts
+++ src/modules/user/dto/change_password.dto.ts
@@ -4,17 +4,17 @@ import { IsOptional, IsString, IsUUID } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class ChangePasswordDto {
-    @ApiPropertyOptional({ example: '550e8400-e29b-41d4-a716-446655440000', description: 'ID của user để thay đổi mật khẩu chỉ admin mới đc chọn không thì lấy trong token' })
+    @ApiPropertyOptional({ example: '550e8400-e29b-41d4-a716-446655440000', description: 'ID of the user to change password if not admin the user_id from token will be used' })
     @IsUUID()
     @IsOptional()
     user_id: string;
 
-    @ApiProperty({ example: 'ABC123', description: 'Mật khẩu mới của user' })
+    @ApiProperty({ example: 'ABC123', description: 'New password of the user' })
     @IsString()
     @IsOptional()
     new_password?: string;
 
-    @ApiProperty({ example: 'XYZ789', description: 'Mật khẩu cũ của user' })
+    @ApiProperty({ example: 'XYZ789', description: 'Old password of the user' })
     @IsString()
     @IsOptional()
     old_password?: string;
--- src/modules/user/dto/create_user.dto.ts
+++ src/modules/user/dto/create_user.dto.ts
@@ -5,53 +5,53 @@ import { ApiProperty } from '@nestjs/swagger';
 import { USER_ROLES } from '../../../constants/constant';
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'Dang Tran', description: 'Tên đầy đủ của user' })
+  @ApiProperty({ example: 'Dang Tran', description: 'Full name of the user' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty({ required: false, example: '123456', description: 'Code của user' })
+  @ApiProperty({ required: false, example: '123456', description: 'Code of the user' })
   @IsString()
   @IsOptional()
   code?: string;
 
-  @ApiProperty({ required: false, example: '2000-01-01', description: 'Ngày sinh của user là dạng string thôi' })
+  @ApiProperty({ required: false, example: '2000-01-01', description: 'Birthday of the user in string format' })
   @IsOptional()
   birthday?: string;
 
-  @ApiProperty({ required: false, example: '0123456789', description: 'Số điện thoại của user' })
+  @ApiProperty({ required: false, example: '0123456789', description: 'Phone number of the user' })
   @IsOptional()
   phone?: string;
 
-  @ApiProperty({ example: 'dangtran@example.com', description: 'Email của user' })
+  @ApiProperty({ example: 'dangtran@example.com', description: 'Email of the user' })
   @IsString()
   email: string;
 
   // @ApiProperty({ example: '123456', description: 'Mật khẩu đăng nhập default là code' })
   // password?: string;
 
-  @ApiProperty({ required: false, example: 'male', description: 'Giới tính của user' })
+  @ApiProperty({ required: false, example: 'male', description: 'Gender of the user' })
   @IsOptional()
   @IsString()
   sex?: string;
 
-  @ApiProperty({ required: false, example: '123 Main St, City, Country', description: 'Địa chỉ của user' })
+  @ApiProperty({ required: false, example: '123 Main St, City, Country', description: 'Address of the user' })
   @IsOptional()
   @IsString()
   address?: string;
 
-  @ApiProperty({ required: false, example: 'user', description: 'Hiện tại có vai trò user' })
+  @ApiProperty({ required: false, example: 'user', description: 'Current role of the user' })
   @IsOptional()
   @IsString()
-  @IsIn(Object.values(USER_ROLES), { message: `Role phải nằm trong: ${Object.values(USER_ROLES).join(', ')}` })
+  @IsIn(Object.values(USER_ROLES), { message: `Role must be one of: ${Object.values(USER_ROLES).join(', ')}` })
   role?: string;
 
-  @ApiProperty({ required: false, description: 'ID chuyên ngành của user' })
+  @ApiProperty({ required: false, description: 'Major ID of the user' })
   @IsOptional()
   @IsUUID()
   major_id?: string;
 
-  @ApiProperty({ required: false, description: 'ID khóa học của user' })
+  @ApiProperty({ required: false, description: 'Major Intake ID of the user' })
   @IsOptional()
   @IsUUID()
   mi_id?: string;
